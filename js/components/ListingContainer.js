9e3ffdf21084b77c9f5c4cb4cd64ce07a515d35b
Katy Morris <katymcconica@gmail.com>
2017-06-20 11:33:39 -0700

    now consoles listings and prints categories

--- js/components/ListingContainer.js
+++ js/components/ListingContainer.js
@@ -25,40 +25,20 @@ var ListingContainer = React.createClass({
 	 //    });
 	 //    return matchingPosts
   // 	},
-  // componentWillMount: function componentWillMount() {
-  //   var th = this;
-  //   console.log("this state")
-  //   console.log(this.state)
-  //   this.serverRequest = axios.get(this.props.categories).then(function (result) {
-  //     for (var i = 0; i < result.data.length; i++) {
-  //     	var propertyName = "cat-" + i
-  //     	var propertyListings = "listing-" + 1
-  //     	var posts = getPostsByCat(result.data[i].id);
-  //     	th.setState({
-  //       	categories: [
-  //       					{
-  //       						[propertyName]: result.data[i].name
-  //       					}
-  //       				]
-  //     	});
-  //     }
-  //   });
-  //     console.log('new state')
-  //     console.log(this.state)
-  // },
-  componentDidMount: function componentDidMount() {
-  	var th = this;
-    console.log("this state")
-    console.log(this.state)
+  componentWillMount: function componentWillMount() {
+ 	var th = this;
+    console.log("this state from mount")
+    console.log(this.state.categories)
     this.serverRequest = axios.get(this.props.categories).then(function (result) {
       var categories = result.data
    	  //loop through each category type - make state for each
    	  var posts = [];
    	  var matchingPosts = [];
+   	  var tempArray = [];
    	  //get all posts
 	  th.serverRequest = axios.get(th.props.posts).then(function (result) {
 	  	posts = result.data
-		for (var i = 0; i < result.data.length - 1; i++) {
+		for (var i = 0; i < result.data.length; i++) {
 			var categoryName = "cat_" + i;
 		  	//loop through all posts to check if this category num matches any of post nums
 		  	for (var j = 0; j < posts.length; j++) {
@@ -69,14 +49,22 @@ var ListingContainer = React.createClass({
 		    		}
 		    	}
 		    }
-		    var newItem = {[categoryName]: {
+		    var newItem =  {
 	    					"cat": categories[i].name,
 	    					"listings": matchingPosts
-	    				  }}
-		    th.state.categories.push(newItem)
+	    				  }
+		    tempArray.push(newItem)
 	      }
+	      th.setState(function(state) {
+		  return {
+		   categories: tempArray
+		  }
+		})
 	  });
     });
+  },
+  componentDidMount: function componentDidMount() {
+  
   },
 
   componentWillUnmount: function componentWillUnmount(th) {
@@ -84,37 +72,58 @@ var ListingContainer = React.createClass({
   },
 
   render: function render() {
-  	console.log("this state")
+  	console.log("this state from render")
     console.log(this.state.categories)
+
     return React.createElement(
       "div", { className: "item-wrapper"}, 
-      this.state.categories.map(function (category, i) {
-      	console.log(category[i])
-	    return React.createElement(
-	        "div",
-	          { className: "wrapper" },
-	          React.createElement(
-	            "div",
-	            { className: "container" },
-	            React.createElement(
-	              "h4",
-	              { className: "post-title" },
-	              React.createElement("a", { href: category.acf.resource_link,
-	                dangerouslySetInnerHTML: { __html: category.acf.name }
-	              })
+       this.state.categories.map(function (category, i) {
+       	console.log("category")
+      	console.log(category.listings)
+	    return 	React.createElement(
+	    		"div",
+	    		{className: "type-wrapper"},
+	    			    	React.createElement(
+	              "h2",
+	              {className: "section-type"},
+	                category.cat
 	            ),
-	            React.createElement(
-	              "div",
-	              { key: category.acf.content, className: "post-content" },
-	                React.createElement("p", {}, category.acf.resource_content)
-	            ),
-	            React.createElement(
-	              "div",
-	              { className: "push" },
-	                React.createElement("div", {})
-	            )
-	          )
-	        )
+		    	React.createElement(
+		    		"div",
+		    		{className: "listings-wrapper"},
+			    	category.listings.map(function (listing, i) {
+			    		console.log("listings")
+			    		console.log(listing)
+			    		return
+			    	})
+		    	)
+	    	)
+	    	// React.createElement(
+	     //    "div",
+	     //      { className: "wrapper" },
+	     //      React.createElement(
+	     //        "div",
+	     //        { className: "container" },
+	     //        React.createElement(
+	     //          "h4",
+	     //          { className: "post-title" },
+	     //          React.createElement("a", { href: category.acf.resource_link,
+	     //            dangerouslySetInnerHTML: { __html: category.acf.name }
+	     //          })
+	     //        ),
+	     //        React.createElement(
+	     //          "div",
+	     //          { key: category.acf.content, className: "post-content" },
+	     //            React.createElement("p", {}, category.acf.resource_content)
+	     //        ),
+	     //        React.createElement(
+	     //          "div",
+	     //          { className: "push" },
+	     //            React.createElement("div", {})
+	     //        )
+	     //      )
+	     //    )
+	    
 	  })
     );
   }
@@ -123,4 +132,4 @@ var ListingContainer = React.createClass({
 
 
 
-ReactDOM.render(React.createElement(ListingContainer, { categories: "http://localhost:8888/self-taught-web-developer/wp-json/wp/v2/categories", posts: "http://localhost:8888/self-taught-web-developer/wp-json/wp/v2/posts"}), document.querySelector("#html-css-resources"));
+ReactDOM.render(React.createElement(ListingContainer, { categories: "http://localhost:8888/self-taught-web-developer/wp-json/wp/v2/categories", posts: "http://localhost:8888/self-taught-web-developer/wp-json/wp/v2/posts"}), document.querySelector("#resources-wrapper"));
--- navigation.php
+++ navigation.php
@@ -1,8 +1,6 @@
 <div class="wrapper nav-wrapper">
 	<div class="container nav-container">
-		<p class="page-title">Self-Taught Web Developer</p>
-		<div class="push"></div>
-		<p class="page-title-sub">Resources</p>
+		<p class="page-title">Web Developer Resources</p>
 		<ul>
 			<a href="http://localhost:8888/self-taught-web-developer/"><li>Home</li></a>
 			<a href="<?php echo get_page_link(26); ?>"><li>Front End Resources</li></a>
